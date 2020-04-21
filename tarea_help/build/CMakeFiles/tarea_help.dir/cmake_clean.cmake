302bc2d066fb641f20c06fc0d7fa8b6f63c0282e
Luis Carbajal <luis.carbajal@utec.edu.pe>
2020-04-20 20:38:11 -0500

    build added

---
+++ tarea_help/build/CMakeFiles/tarea_help.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tarea_help.dir/src/main.cpp.obj"
+  "libtarea_help.dll.a"
+  "tarea_help.exe"
+  "tarea_help.exe.manifest"
+  "tarea_help.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tarea_help.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
